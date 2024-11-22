23ba9c945a677de43dd4d8700117a66a58dbac62
Vlad-Andrei Gruia <58209761+vladandrei228@users.noreply.github.com>
2024-11-22 04:43:23 +0100

    update time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // Set the date and time when the element should be shown
-const showDateTime = new Date('2024-11-22T4:39:00');
+const showDateTime = new Date('2024-11-23T04:46:00+01:00');
 
 // Function to check the current date and time
 function checkDateTime() {
